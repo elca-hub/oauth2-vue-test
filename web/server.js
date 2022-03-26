885b72cd2fc0dc29efb6dccd3d3595d5b4f8f22a
poyuaki <101086816+m0k1moki@users.noreply.github.com>
2022-03-26 16:38:37 +0900

    spaに対応

--- web/server.js
+++ web/server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static')
+const history = require('connect-history-api-fallback')
 
 const app = express()
+app.use(history())
 app.use(serveStatic(path.join(__dirname, 'dist')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 const port = 8080
 
